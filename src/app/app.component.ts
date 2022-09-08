5a4a7a96cc970709499f3638fa4d42c3bbddd5ba
miraj <miraj00@gmail.com>
2022-09-07 23:31:08 -0400

    code added to show weather putput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ApiService } from './api.service';
 export class AppComponent {
   title = 'weatherAPI';
 
+  
   constructor(private apiService:ApiService){
 
   }
@@ -17,9 +18,47 @@ export class AppComponent {
     this.getApiResponse();
   }
 
+
+  name0 :string ="";
+  temperature0 :string='';
+  icon0:string = '';
+  detailInfo0:string ="";
+
+  name :string ="";
+  temperature :string='';
+  icon:string = '';
+  detailInfo:string ="";
+
   getApiResponse() : void {
     this.apiService.getWeather()
-    .subscribe((data) => { console.log(data)  });
+    .subscribe((data) => {     
+                            //----- First Forcast ------                      
+                            this.name0 = data.properties.periods[0].name;
+                            this.temperature0 = data.properties.periods[0].temperature;
+                            this.icon0 = data.properties.periods[0].icon;
+                            this.detailInfo0 = data.properties.periods[0].detailedForecast;
+                     
+                            // console logging just to check output values          
+                            console.log(this.name0);
+                            console.log(this.temperature0);
+                            console.log(this.icon0);
+                            console.log(this.detailInfo0);
+                         
+                            //------ Second Forcast---------
+
+                            this.name = data.properties.periods[1].name;
+                            this.temperature = data.properties.periods[1].temperature;
+                            this.icon = data.properties.periods[1].icon;
+                            this.detailInfo = data.properties.periods[1].detailedForecast;
+                              
+                            // console logging just to check output values  
+                            console.log(this.name);
+                            console.log(this.temperature);
+                            console.log(this.icon);
+                            console.log(this.detailInfo);
+                           
+                            
+                        });
 
   }
 }
