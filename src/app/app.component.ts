ecf35ae87e8cbf89de00034aa5fc9f577aa6fdcb
miraj <miraj00@gmail.com>
2022-09-07 21:39:11 -0400

    response data present in console log at this point in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weatherAPI';
+
+  constructor(private apiService:ApiService){
+
+  }
+
+  ngOnInit(){
+    this.getApiResponse();
+  }
+
+  getApiResponse() : void {
+    this.apiService.getWeather()
+    .subscribe((data) => { console.log(data)  });
+
+  }
 }
